e5fa7ee4b3e582d7812baedc0c3239994c280adc
Manjunadha949 <110009166+Manjunadha949@users.noreply.github.com>
2024-05-21 20:20:06 +0530

    Add files via upload

---
+++ HallBooking/Approve.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HallBooking
+{
+    public partial class Approve : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           String  username = Session["username"].ToString();
+            Response.Write("Welcome " + username);
+
+        }
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
